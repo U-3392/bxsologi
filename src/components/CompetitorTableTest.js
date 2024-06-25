749ada7ef8fe36bd001c0a575d29f1ee9e346fd6
megahertz <27af6.mhz@gmail.com>
2024-06-24 22:27:41 -0400

    minor fix

--- src/components/CompetitorTableTest.js
+++ src/components/CompetitorTableTest.js
@@ -1,6 +1,34 @@
-simpleApp.controller('aboutController', function($scope, $http) {
-    $http.get('data.json').then(function(response) {
-     $scope.data = response.data;
-    });
-    
-  });
+import React from 'react'
+import JsonData from './data.json'
+ function JsonDataDisplay(){
+    const DisplayData=JsonData.map(
+        (info)=>{
+            return(
+                <tr>
+                    <td>{info.name}</td>
+                    <td>{info.grade}</td>
+                    <td>{info.competitions}</td>
+                </tr>
+            )
+        }
+    )
+ 
+    return(
+        <div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th>Grade</th>
+                    <th>Placements</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {DisplayData}
+                </tbody>
+            </table>
+        </div>
+    )
+ }
+ 
+ export default JsonDataDisplay;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { DatabaseTable, createTable} from "../components/CompetitorTable";
- 
+import JsonDataDisplay from "../components/CompetitorTableTest";
 const Search = () => {
     return (
         <div>
@@ -12,16 +12,7 @@ const Search = () => {
                     This section obtains the competition results, which are stored in a database. You can query for certain categories -- for example, you can search for "Top 5%" in "All events." 
                 </i>
             </p>
-            <tbody>
-        <tr>
-          <th>Name</th>
-          <th>Grade</th>
-          <th>Initial Diagnostic</th>
-          <th>Rickards Invitational</th>
-          <th>Boyceville Invitational</th>
-          <th>Regionals Diagnostic</th>
-        </tr>
-      </tbody>
+            <JsonDataDisplay />
         </div>
     );
 };
