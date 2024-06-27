e79d329429ab8c2b78207dad8965ed23488c26b7
U-3392 <27af6.mhz@gmail.com>
2024-06-27 13:33:08 -0400

    s

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/" activeStyle>
+                        Home
+                    </NavLink>
+                    <NavLink to="/documentation" activeStyle>
+                        Documentation
+                    </NavLink>
+                    <NavLink to="/search" activeStyle>
+                        Competitor Search
+                    </NavLink>
+                    <NavLink to="/roster" activeStyle>
+                        Competition Roster Creator
+                    </NavLink>
+                    <NavLink to="/simulation" activeStyle>
+                        Competition Simulation
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
